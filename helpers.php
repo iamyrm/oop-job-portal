5ad5d40c7eda4d07d264f49a0b1d4b0c0099d9e6
iamyrm <majhiyagya123@gmail.com>
2025-09-03 16:56:49 +0545

    Partial seperated

--- helpers.php
+++ helpers.php
@@ -1,13 +1,31 @@
 <?php
 
-/**
- * Get the base path
- * 
- * @param string $path
- * @return string
- */
-
+// Base path function 
 function basePath($path = '')
 {
    return __DIR__ . '/' . $path;
+}
+
+// loading views
+function loadView($name)
+{
+   $viewPath = basePath("views/{$name}.view.php");
+
+   if (file_exists($viewPath)) {
+      require $viewPath;
+   } else {
+      echo "View '{$name} not found!'";
+   }
+}
+
+// loading partials
+function loadPartials($name)
+{
+   $partialPath = basePath("views/partials/{$name}.php");
+
+   if (file_exists($partialPath)) {
+      require $partialPath;
+   } else {
+      echo "Partial '{$name} is not found!'";
+   }
 }
