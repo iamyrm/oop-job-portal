43390c3976e783049b5d9ae5e4a598f55ec5b578
iamyrm <majhiyagya123@gmail.com>
2025-09-09 11:47:55 +0545

    Database connection done

---
+++ Database.php
@@ -0,0 +1,21 @@
+<?php
+class Database
+{
+   public $conn;
+
+   public function __construct($config)
+   {
+      $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']}";
+
+      $options = [
+         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+      ];
+
+      try {
+         $this->conn = new PDO($dsn, $config['username'], $config['password'], $options);
+      } catch (PDOException $e) {
+         throw new Exception("Database Connection Failed: {$e->getMessage()}");
+      }
+   }
+}
--- public/index.php
+++ public/index.php
@@ -1,9 +1,14 @@
 <?php
 require '../helpers.php';
 
+require basePath('Database.php');
+$config = require basePath('config/db.php');
+$db = new Database($config);
+
 require basePath('Router.php');
 $router = new Router();
-require basePath('routes.php');
+
+$routes = require basePath('routes.php');
 
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
